3d68afd733684161573cad4f3c234cbcaa045c3d
emilliman5 <emilliman4@gmail.com>
2015-05-27 13:08:33 -0400

    completed script to perform basic freq. analysis and generate a word cloud.

--- text_mine.R
+++ text_mine.R
@@ -1,6 +1,7 @@
 library(XML)
 library(tm)
 library(SnowballC)
+library(wordcloud)
 
 setwd("~/workspace/Pubmed_mining/")
 pubmed<-xmlTreeParse("../../Downloads/pubmed_result.xml",useInternalNodes = T)
@@ -9,23 +10,46 @@ top<-xmlRoot(pubmed)
 abstr<-xpathApply(top, "//MedlineCitation/Article/Abstract", xmlValue)
 abstr.df<-do.call("rbind", abstr)
 
-abstrCorpus<-Corpus(VectorSource(abstr.df))
+abstrCorpus<-Corpus(DataframeSource(abstr.df))
 
-abstrCorpus<-tm_map(abstrCorpus, tolower)
+abstrCorpus<-tm_map(abstrCorpus, content_transformer(tolower))
 abstrCorpus<-tm_map(abstrCorpus, removePunctuation)
 abstrCorpus<-tm_map(abstrCorpus, removeNumbers)
 
 myStopwords<-c(stopwords('english'), "available", "via")
+
+#a specific set of stopwords will need to be developed for this analysis
+
 ##"r" does not exist in the stopwords vector...
 #myStopwords<-myStopwords[-which(myStopwords=="r")]
 
 abstrCorpus<-tm_map(abstrCorpus, removeWords, myStopwords)
-dictCorpus<-abstrCorpus
 abstrCorpus<-tm_map(abstrCorpus, stemDocument)
+dictCorpus<-abstrCorpus
+abstrCorpus<-tm_map(abstrCorpus, stripWhitespace)
 inspect(abstrCorpus[1:3])
 
-##stemCompletion func fails for unknown reason
-##abstrCorpus<-tm_map(abstrCorpus, stemCompletion, type="first", dictionary=dictCorpus)
+##stemCompletion breaks corpus...
+#abstrCorpus<-tm_map(abstrCorpus, stemCompletion, dictionary=dictCorpus)
+#inspect(abstrCorpus[1:3])
 
-##Term Document matrix func call fails.
-myDtm<-TermDocumentMatrix(abstrCorpus)
+tdm<-TermDocumentMatrix(abstrCorpus)
+inspect(tdm[1:10,1:10])
+
+################
+##Some basic analyses
+################
+
+findFreqTerms(tdm,lowfreq = 500)
+findAssocs(tdm,terms = c("human", "risk", "exposur"), corlimit = 0.25)
+
+tdm.m<-as.matrix(tdm)
+tdm.s<-sort(rowSums(tdm.m), decreasing = T)
+myNames<-names(tdm.s)
+
+##Word cloud :-)
+
+tdm.df<-data.frame(word=myNames, freq=tdm.s)
+png("wordCloud.png", height=800, width=800, units="px")
+wordcloud(tdm.df$word, tdm.df$freq, min.freq = 500)
+dev.off()
