c993a10f102014cd0bc7cf8bddc9c46a0ca318fd
emilliman5 <emilliman4@gmail.com>
2015-05-28 07:25:08 -0400

    added function to produce a standard date format

--- textMine_funcs.R
+++ textMine_funcs.R
@@ -3,3 +3,10 @@ stemCompletion_mod <- function(x,dict=dictCorpus)
   PlainTextDocument(stripWhitespace(paste(stemCompletion(unlist(strsplit(as.character(x)," ")),
                                                          dictionary=dict),sep="", collapse=" ")))
 }
+
+getDate<-function()
+{
+  ## Purpose is to define a data in the format
+  ## 2015may05 (YYYYmmDD)
+  tolower(format(Sys.time(), "%Y%b%d"));
+}
