15380c7a3b6e312b1eb85743e5177799ab809e82
emilliman5 <emilliman4@gmail.com>
2015-05-28 13:15:32 -0400

    Added second stemcompletion function for possible workaround

--- textMine_funcs.R
+++ textMine_funcs.R
@@ -10,3 +10,14 @@ getDate<-function()
   ## 2015may05 (YYYYmmDD)
   tolower(format(Sys.time(), "%Y%b%d"));
 }
+
+stemCompletion2 <- function(x, dictionary) 
+{
+  
+  x <- unlist(strsplit(as.character(x), " "))  
+  x <- x[x != ""]  
+  x <- stemCompletion(x, dictionary=dictionary)
+  x <- paste(x, sep="", collapse=" ") 
+  PlainTextDocument(stripWhitespace(x))
+  
+}
