353a1f0aec5145a0ca24a5dc446d4a3dbf9401cc
emilliman5 <emilliman4@gmail.com>
2015-05-28 22:44:51 -0400

    reorganized result directory calls, minor changes to explore data

--- text_mine.R
+++ text_mine.R
@@ -14,8 +14,8 @@ if (file.exists(extraFunFile)) {
 }
 
 dir.create("results/")
-dir.create(paste0("results/",getDate()))
 resultsPath<-paste0("results/",getDate())
+dir.create(resultsPath)
 
 pubmed<-xmlTreeParse("pubmed_result.xml",useInternalNodes = T)
 top<-xmlRoot(pubmed)
@@ -26,7 +26,8 @@ abstr.df<-do.call("rbind", abstr)
 abstrCorpus<-Corpus(DataframeSource(abstr.df))
 
 keywords<-xpathApply(top, "//KeywordList", xmlValue)
-keywords<-do.call("rbind",keywords)
+keywords.df<-do.call("rbind",keywords)
+abstrCorpus<-Corpus(DataframeSource(keywords.df))
 
 mesh<-xpathApply(top, "//MeshHeadingList", xmlValue)
 mesh.df<-do.call("rbind",mesh)
@@ -46,7 +47,7 @@ abstrCorpus<-tm_map(abstrCorpus, stemDocument)
 abstrCorpus<-tm_map(abstrCorpus, stripWhitespace)
 inspect(abstrCorpus[1:3])
 
-tmpCorpus<-mclapply(abstrCorpus, stemCompletion2, dictionary=dictCorpus, mc.cores=4)
+tmpCorpus<-mclapply(abstrCorpus, stemCompletion2, dictionary=dictCorpus, mc.cores=6)
 tmpCorpus<-Corpus(VectorSource(tmpCorpus))
 abstrCorpus<-tmpCorpus
 #inspect(abstrCorpus[1:3])
@@ -58,15 +59,15 @@ inspect(tdm[100:200,1:10])
 ##Some basic analyses
 ################
 
-findFreqTerms(tdm,lowfreq = 50)
-findAssocs(tdm,terms = c("tumorcell", "cell", "pollutants"), corlimit = 0.25)
+findFreqTerms(tdm,lowfreq = 10)
+findAssocs(tdm,terms = c("particulate", "toxic", "air"), corlimit = 0.25)
 
 tdm.m<-as.matrix(tdm)
 tdm.s<-sort(rowSums(tdm.m), decreasing = T)
 myNames<-names(tdm.s)
 
-term.freq<-subset(tdm.s, tdm.s>=50)
-freq.terms<-findFreqTerms(tdm, lowfreq=50)
+term.freq<-subset(tdm.s, tdm.s>=10)
+freq.terms<-findFreqTerms(tdm, lowfreq=10)
 png(paste0(resultsPath,"/Top25_word_graph.png"), height=1200, width=1600, units="px")
 plot(tdm, term=freq.terms, corThreshold = 0.1, weighting=T)
 dev.off()
@@ -75,10 +76,10 @@ dev.off()
 
 tdm.df<-data.frame(word=myNames, freq=tdm.s)
 png(paste0(resultsPath,"/wordCloud.png"), height=800, width=800, units="px")
-wordcloud(tdm.df$word, tdm.df$freq, min.freq = 20, colors=brewer.pal(9, "BuGn"), random.order=F)
+wordcloud(tdm.df$word, tdm.df$freq, min.freq = 2, colors=brewer.pal(9, "BuGn"), random.order=F)
 dev.off()
 
-tdm2<-removeSparseTerms(tdm, sparse = 0.99)
+tdm2<-removeSparseTerms(tdm, sparse =0.995)
 tdm2.m<-as.matrix(tdm2)
 distMatrix<-dist(dist(scale(tdm2.m)))
 fit<-hclust(distMatrix,method = "ward.D")
