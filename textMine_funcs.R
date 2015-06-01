73e7a6fb328c9c94a7dbdd7fdcd489d7878639d2
emilliman5 <emilliman4@gmail.com>
2015-06-01 18:03:26 -0400

    treaked stemCompletion code, gained ~25%. Tested with microbenchmark 10x, with at ~5300 entry corpus.

--- textMine_funcs.R
+++ textMine_funcs.R
@@ -10,7 +10,7 @@ stemCompletion2 <- function(x, dictionary)
   
   x <- unlist(strsplit(as.character(x), " "))  
   x <- x[x != ""]  
-  x <- stemCompletion(x, dictionary=dictionary)
+  x <- stemCompletionF(x, dictionary=dictionary)
   x <- paste(x, sep="", collapse=" ") 
   PlainTextDocument(stripWhitespace(x))
   
@@ -19,4 +19,43 @@ stemCompletion2 <- function(x, dictionary)
 NgramTokenizer <-function(x,n=2)
 {
   unlist(lapply(ngrams(words(x), n), paste, collapse = " "), use.names = FALSE)
+}
+
+stemCompletionF<-function (x, dictionary, 
+          type = c("prevalent", "first", "longest", "none", "random", "shortest")) 
+{
+ 
+  if (inherits(dictionary, "Corpus")) 
+    dictionary <- unique(unlist(lapply(dictionary, words)))
+  type <- match.arg(type)
+  y<-unique(x)
+  possibleCompletions <- lapply(y, function(w) grep(sprintf("^%s", 
+                                                            w), dictionary, value = TRUE))
+  switch(type, first = {
+    setNames(sapply(possibleCompletions, "[", 1), x)
+  }, longest = {
+    ordering <- lapply(possibleCompletions, function(x) order(nchar(x), 
+                                                              decreasing = TRUE))
+    possibleCompletions <- mapply(function(x, id) x[id], 
+                                  possibleCompletions, ordering, SIMPLIFY = FALSE)
+    setNames(sapply(possibleCompletions, "[", 1), x)
+  }, none = {
+    setNames(x, x)
+  }, prevalent = {
+    possibleCompletions <- lapply(possibleCompletions, function(x) sort(table(x), 
+                                                                        decreasing = TRUE))
+    n <- structure(names(sapply(possibleCompletions, "[", 1)))
+    n[match(x,names(n))]
+    #setNames(if (length(n)) n else rep(NA, length(x)), x)
+  }, random = {
+    setNames(sapply(possibleCompletions, function(x) {
+      if (length(x)) sample(x, 1) else NA
+    }), x)
+  }, 
+  shortest = {
+    ordering <- lapply(possibleCompletions, function(x) order(nchar(x)))
+    possibleCompletions <- mapply(function(x, id) x[id], 
+                                  possibleCompletions, ordering, SIMPLIFY = FALSE)
+    setNames(sapply(possibleCompletions, "[", 1), x)
+  })
 }
