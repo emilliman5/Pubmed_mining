6d58616a77214b96ea531621870a0a391fa06072
emilliman5 <emilliman4@gmail.com>
2015-06-05 10:11:13 -0400

    Add standard data transforms to SP corpus analysis. Currently broken.

--- SP_analysis.R
+++ SP_analysis.R
@@ -15,8 +15,24 @@ if (file.exists(extraFunFile)) {
 files<-list.files(pattern = "SP_")
 files<-files[grep(".txt", files)]
 
-docs<-do.call("rbind", lapply(files, function(x) paste(readLines(file(x)), collapse=" ")))
-
+docs<-do.call("rbind", lapply(files, function(x) paste(readLines(file(x, open="r")), collapse=" ")))
 docs<-cbind(gsub(".txt", "",files), docs)
 
 SP<-VCorpus(VectorSource(docs[,2]))
+SP<-tm_map(SP, content_transformer(tolower))
+SP<-tm_map(SP, removePunctuation)
+SP<-tm_map(SP, removeNumbers)
+
+stopWords<-read.table("stopwords.txt")
+myStopwords<-c(stopwords('english'), stopWords$V1)
+
+SP<-tm_map(SP, removeWords, myStopwords)
+dictCorpus<-SP
+SP<-tm_map(SP, stemDocument)
+SP<-tm_map(SP, stripWhitespace)
+
+SP<-mclapply(SP, stemCompletion2, dictionary=dictCorpus, mc.cores=24)
+SP<-Corpus(VectorSource(SP))
+
+tdm<-TermDocumentMatrix(SP)
+inspect(tdm)
