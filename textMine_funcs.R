5afe12944a86c64b7eef5f01a43e716e413687de
emilliman5 <emilliman4@gmail.com>
2015-06-16 12:01:45 -0400

    minor update to comments

--- textMine_funcs.R
+++ textMine_funcs.R
@@ -1,7 +1,7 @@
 getDate<-function()
 {
   ## Purpose is to define a data in the format
-  ## 2015may05 (YYYYmmDD)
+  ## 2015may05_1631 (YYYYmmDD_HHmm)
   tolower(format(Sys.time(), "%Y%b%d_%H%M"));
 }
 
