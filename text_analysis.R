a4fd1b3cddfce8fd11518ff57dc033ed03efd2c3
emilliman5 <emilliman4@gmail.com>
2015-06-22 18:44:50 -0400

    added FY subset and looping to different plots

--- text_analysis.R
+++ text_analysis.R
@@ -5,6 +5,9 @@ library(Rgraphviz)
 library(topicmodels)
 library(lubridate)
 
+#If you want to force a reprocessing of the documents into a Corpus set this value to "TRUE"
+reset<-FALSE
+
 extraFunFile<-"textMine_funcs.R"
 if (file.exists(extraFunFile)) {
   source(extraFunFile, keep.source=TRUE);
@@ -26,7 +29,13 @@ if(!file.exists("Corpus/1.txt") || reset){
 }
 
 
-spCorpus<-Corpus(DirSource("Corpus/SP/"), readerControl = list(language="english"))
+
+if(!file.exists("Corpus/SP/SP_Goal1") || reset){
+  ##call function to read and process corpus
+} else {
+  ##read in Strategic Plan corpus
+  spCorpus<-Corpus(DirSource("Corpus/SP/"), readerControl = list(language="english"))
+}
 
 abstrCorpus<-c(abstrCorpus, spCorpus)
 
@@ -45,7 +54,7 @@ tdm.monogram<-TermDocumentMatrix(abstrCorpus,
 tdm.bigram <- TermDocumentMatrix(abstrCorpus, control = list(tokenize = NgramTokenizer))
 ##function(x) weightTfIdf(x,normalize=F)))
 
-##Run one of the following commands before proceding. 
+##Run one of the following commands before proceeding. 
 ##tdm.monogram are single term frequencies.
 ##tdm.bigram are two term frequencies
 
@@ -56,18 +65,17 @@ tdm<-tdm.bigram
 ##TermFreq exploration
 ###########
 tdm
+FYs<-unique(meta(abstrCorpus)$FY)
+x<-lapply(FYs, function(x) which(meta(abstrCorpus)$FY==x) )
 
-tdm.m<-as.matrix(tdm)
-tdm.s<-sort(rowSums(tdm.m), decreasing = T)
-idf<-apply(tdm.m, 1, function(x) length(x)/sum(x>=1))
-
-png(file.path(resultsPath,"TermFreq_Distributions.png", fsep = "/"), height=4000, width=2400, units="px")
-par(mfrow=c(3,1), cex=4)
-hist(sqrt(rowSums(tdm.m)), breaks=100, col="blue4", main="Distribution of terms in Corpus")
-hist(sqrt(rowMeans(tdm.m)), breaks=100, col="red", main="Distribution of Avg. term usage per Document in Corpus")
-hist(idf, breaks=100, col="green",main="Distribution of idf Scores")
-dev.off()
-
+lapply(x, function(y){
+  png(file.path(resultsPath,paste0(meta(abstrCorpus)[y[1],"FY"], "_TermFreq_Distributions.png"), fsep = "/"), 
+      height=800, width=1200, units="px")
+  par(mfrow=c(2,1), cex=2)
+  hist(sqrt(rowSums(as.matrix(tdm[,y]))), breaks=100, col="blue4", main="Distribution of term Scores in Corpus")
+  hist(sqrt(rowMeans(as.matrix(tdm[,y]))), breaks=100, col="red", main="Distribution of Avg. term score per Document in Corpus")  
+  dev.off()
+  })
 
 ################
 ##Parameters
@@ -104,10 +112,10 @@ dev.off()
 
 ##Word cloud
 
-sapply(as.factor(meta(abstrCorpus)[,"FY"]), function(x) {
-  tdm.s<-tdm[,which(as.factor(meta(abstrCorpus)[,"FY"])==x)]
-  tdm.df<-data.frame(word=rownames(tdm.s), freq=tdm.s)
-  png(paste0(resultsPath,"/",x,"_wordCloud.png"), height=1600, width=1600, units="px")
+lapply(x, function(y) {
+  low<-quantile(rowSums(as.matrix(tdm[,y])), probs = 0.99)
+  tdm.df<-data.frame(word=rownames(tdm), freq=rowSums(as.matrix(tdm)[,y]))
+  png(paste0(resultsPath,"/",paste0(meta(abstrCorpus)[y[1],"FY"],"_wordCloud.png")), height=1600, width=1600, units="px")
   wordcloud(tdm.df$word, tdm.df$freq, scale=c(15,0.5), min.freq = low, colors=brewer.pal(9, "BuGn")[-(1:4)], random.order=F)
   dev.off()
 })
