c56691b0cd845d49c608f0b408d1888855ab84a6
emilliman5 <emilliman4@gmail.com>
2015-06-23 11:03:25 -0400

    added function to make corpus from strategic plan

--- makeCorpus.R
+++ makeCorpus.R
@@ -1,16 +1,17 @@
-makeCorpus<-function(pub.file, stopword="stopwords.txt", cores=30){
+makeCorpus<-function(pub.file, stopwordList="stopwords.txt", cores=30){
 
   library(XML)
   library(tm)
   library(SnowballC)
   library(lubridate)
+  library(parallel)
   
   extraFunFile<-"textMine_funcs.R"
   if (file.exists(extraFunFile)) {
     source(extraFunFile, keep.source=TRUE);
   } else{ break }
   
-  stopWords<-read.table(stopword, colClasses = c("character"))
+  stopWords<-read.table(stopwordList, colClasses = c("character"))
   myStopwords<-c(stopwords('english'), stopWords$V1)
   
   pubmed<-xmlParse(pub.file,useInternalNodes = T)
@@ -44,10 +45,44 @@ makeCorpus<-function(pub.file, stopword="stopwords.txt", cores=30){
   abstrCorpus<-Corpus(VectorSource(abstrCorpus))
   meta(abstrCorpus, "GrantID")<-abstr.df[,"GrantID"]
   meta(abstrCorpus, "Date")<-abstr.df[,"pubdate.df"]
-  meta(abstrCorpus, "FY")<-quarter(abstr.df[,"pubdate.df"]+90, with_year=T)
+  meta(abstrCorpus, "FY.Q")<-quarter(abstr.df[,"pubdate.df"]+90, with_year=T)
+  meta(abstrCorpus, "FY")<-floor(meta(abstrCorpus)$FY)
   
   dir.create("Corpus")
   writeCorpus(abstrCorpus,"Corpus/")
   write.csv(meta(abstrCorpus), "CorpusMetaData.txt",row.names=F)
   abstrCorpus
 } 
+
+makeSPCorpus<-function(SP_path="data/Strategic_goals",stopwordList="stopwords.txt", pat=c("Goal","Theme","all")){
+  files<-list.files(pattern = "SP_",path=SP_path,full.names = T)
+  if(pat!="all"){
+    files<-files[grep(pat, files)]
+  }
+  
+  stopWords<-read.table(stopwordList, colClass=c("character"))
+  myStopwords<-c(stopwords('english'), stopWords$V1)
+  
+  docs<-do.call("rbind", lapply(files, function(x) paste(readLines(file(x)), collapse=" ")))
+  docs<-cbind(gsub("data/Strategic_goals//","",gsub(".txt", "",files)), docs)
+  
+  SP<-VCorpus(VectorSource(docs[,2]))
+  SP<-tm_map(SP, toSpace, "/|@|\\||-|_|\\\\")
+  SP<-tm_map(SP, removePunctuation)
+  SP<-tm_map(SP, removeNumbers)
+  
+  SP<-tm_map(SP, removeWords, myStopwords)
+  dictCorpus<-SP
+  SP<-tm_map(SP, stemDocument)
+  SP<-tm_map(SP, stripWhitespace)
+  SP<-tm_map(SP,content_transformer(tolower))
+  
+  SP<-mclapply(SP, stemCompletion2, dictionary=dictCorpus, mc.cores=cores)
+  SP<-Corpus(VectorSource(SP))
+  meta(SP, "id")<-docs[,1]
+  
+  dir.create("Corpus/SP/")
+  writeCorpus(SP,path = "Corpus/SP",filenames = meta(SP)[,1])
+  SP
+  
+}
