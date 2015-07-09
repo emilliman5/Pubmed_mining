2319c7d2763f4a06f4bfb531c6558898269583db
emilliman5 <emilliman4@gmail.com>
2015-07-09 19:32:38 -0400

    removed low freq words before LDA for simulation

--- topicModeling.R
+++ topicModeling.R
@@ -1,23 +1,20 @@
 library(parallel)
 
 idx<-getFactorIdx("FY",meta(abstrCorpus))
-idx<-do.call(c, lapply(idx, function(x) sample(x, 10)))
+idx<-do.call(c, lapply(idx, function(x) sample(x, 50)))
 
 test.corpus<-abstrCorpus[idx]
 test.dtm<-DocumentTermMatrix(test.corpus)
 
-best.model<-mclapply(seq(2,1000,2),mc.cores = 12, function(k) LDA(test.dtm, k) )
+dtm<-t(t(as.matrix(test.dtm))[as.vector(apply(t(as.matrix(test.dtm)), 1, sum)>10),])
+
+seq.k<-seq(2,200,10)
+
+best.model<-mclapply(seq.k,mc.cores = 10, function(k) LDA(test.dtm, k) )
 best.model.lglk<-as.data.frame(as.matrix(lapply(best.model, logLik)))
-LogLik.df<-data.frame("topics"=seq(2,200,2), 
+LogLik.df<-data.frame("topics"=seq.k, 
                     "LL"=as.numeric(as.matrix(best.model.lglk)))
 
-sp.dtm<- DocumentTermMatrix(spCorpus)
-lda.sp<-mclapply(seq(2,30,1), function(k) LDA(sp.dtm, k), mc.cores = 30)
-best.lda<-as.data.frame(as.matrix(lapply(lda.sp, logLik)))
-best.lda<-data.frame(topics=seq(2,30,1), LL=as.numeric(as.matrix(best.lda)))
-
 png(paste(resultsPath,"LDA_topicNumber_optimziation.png", sep="/"), height=1200, width=1200, units="px")
-par(mfrow=c(2,1))
-plot(best.lda$LL~best.lda$topics,pch=19, col="blue", main="LDA Simulation with Strategic Plan")
 plot(LogLik.df$LL~LogLik.df$topics, pch=19, col="red", main="LDA Simulation with 10 docs per FY")
 dev.off()
