c3ddc24820690ec50fbcc62ae9cc2584470abc51
emilliman5 <emilliman4@gmail.com>
2015-07-15 08:53:44 -0400

    ditto

--- xml_testing.R
+++ xml_testing.R
@@ -1,15 +1,10 @@
 abstr.df<-do.call("rbind", xpathApply(top, "//PubmedArticle/MedlineCitation/Article", function(node)
 {
-    grantID<-xmlValue(node[['GrantList']][['Grant']][['GrantID']],recursive = T)
     title<-xmlValue(node[['ArticleTitle']])
     abstr<-xmlValue(node[['Abstract']][['AbstractText']])
     data.frame("GrantID"=grantID, "Title"=title, "Abstract"=abstr, stringsAsFactors=F)
 } ))
 
-grantIDs<-xmlApply(top[['PubmedArticle']][['MedlineCitation']], function(x){
-    getNodeSet(x, "//GrantList/Grant/GrantID")
-})
 
-g<-xmlSApply(gn, xmlValue)
-
-gn<-getNodeSet(top, "//GrantList")
+grantNodes<-getNodeSet(top, "//GrantList")
+grantID<-xmlSApply(grantNodes, function(x) xpathApply(x, "//GrantID", xmlValue))
