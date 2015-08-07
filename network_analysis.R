8e6753c061f56f415d5da2c31d0c3d2361f46053
emilliman5 <emilliman4@gmail.com>
2015-08-07 17:52:37 -0400

    added phylogram plot to complement dendrogram. added better network visuaization for one FY.Q need to extend to all time points.

--- network_analysis.R
+++ network_analysis.R
@@ -1,27 +1,78 @@
 library(igraph)
+library(ape)
+#setwd("results/2015aug06_0840/")
 
-setwd("results/2015aug06_0840/")
+dends1<-lapply(names(topDocDist.fy), function(x){
+    lapply(seq(1,length(topDocDist.fy[[x]])), function(y){
+        hclust(topDocDist.fy[[x]][[y]])
+    })
+})
 
+x<-dends1[[1]][[2]]
+phyloTree<-as.phylo(x)
+phyloEdges<-phyloTree$edge    
+net<-graph.edgelist(phyloEdges,directed = F)     
+plot(net)
 
+graphLayout<-layout.auto(net)
+plot(net, layout = graphLayout*0.2, rescale=F)
+n.obs<-length(x$labels)
+png(paste0(resultsPath, "/topic_phylo.png"), height=2400, width=2400, units="px")
+plot(graphLayout[,1],graphLayout[,2], type="n", axes=F, xlab="", ylab="")
+segments(
+    x0 = graphLayout[phyloEdges[,1],1], 
+    y0 = graphLayout[phyloEdges[,1],2],
+    x1 = graphLayout[phyloEdges[,2],1],
+    y1 = graphLayout[phyloEdges[,2],2],
+    col = "darkgray", lwd = 3
+)
+
+text(graphLayout[1:n.obs,1],graphLayout[1:n.obs,2],
+    phyloTree$tip.label, cex=2, xpd=T, font=1)
+dev.off()
 
 ############
 ##Static Network Output
 ############
-nodes<-read.csv("results//2015aug05_0846//Network//Topics50//TopicNodes.csv", stringsAsFactors=F)
-edges<-read.csv("results//2015aug05_0846//Network//Topics50//TopicTopicbyDocSimilarity.csv", stringsAsFactors=F)         
+TopicNodes<-read.csv("results//2015aug05_0846//Network//Topics50//TopicNodes.csv", stringsAsFactors=F)
+DocTopEdges<-read.csv("results//2015aug05_0846//Network//Topics50/TopicDocumentProbEdges.csv", stringsAsFactors=F)         
+DocNodes<-read.csv("results/2015aug05_0846/Network/AbstrNodeAttrs.csv", stringsAsFactors=F)
+SpNodes<-read.csv("results/2015aug05_0846/Network/SpNodeAttrs.csv", stringsAsFactors=F)
 
-nodes<-nodes[,c("ID","TopicWords","Type")]
-colnames(edges)<-c("from","to","Weight","Type")          
-edges<-edges[edges$Weight<0.9,]
+TopicNodes[,c("FY","FY.Q")]<-rep(NA)
 
-sum(edges$Weight<0.9)
-length(edges$Weight)
+SpNodes[,c("FY","FY.Q")]<-NA
+SpNodes[,"TopicWords"]<-SpNodes[,"ID"]
+DocNodes[,c("TopicWords")]<-NA
+
+TopicNodes<-TopicNodes[,c("ID","TopicWords","Type","FY","FY.Q")]
+DocNodes<-DocNodes[,c("ID","TopicWords","Type","FY","FY.Q")]
+SpNodes<-SpNodes[,c("ID","TopicWords","Type","FY","FY.Q")]
+
+nodes<-rbind(TopicNodes,DocNodes,SpNodes)
+
+colnames(DocTopEdges)<-c("from","to","Weight","Type")          
+
+nodes<-rbind(TopicNodes,DocNodes[DocNodes$FY.Q==2009.1,],SpNodes)
+edges<-DocTopEdges[DocTopEdges$from %in% nodes$ID,]
+edges<-do.call(rbind, by(edges, edges$from, function(x) x[order(x$Weight, decreasing=T)[1:3],]))
+edges<-edges[!is.na(edges$Weight),]
+
 
 net<-graph.data.frame(edges, nodes, directed=F)
-plot(net, vertex.label=NA, edge.width=((E(net)$Weight*30)-22), edge.curved=0.2,
-     layout=layout.fruchterman.reingold)
+V(net)$TopicWords<-gsub("\\|", ",", V(net)$TopicWords)
 
-l<-layout.fruchterman.reingold(net, repulserad=vcount(net)^3)
+l<-layout.fruchterman.reingold(net, weight=E(net)$Weight*10,repulserad=vcount(net)^4)
+shape<-factor(nodes$Type,labels = c("circle","square","csquare"))
+color<-factor(nodes$Type,labels = c("blue","chartreuse4","red"))
 
-plot(net, vertex.label=NA, edge.width=8/E(net)$Weight-7, edge.curved=0.2,
-     layout=l)
+png(paste0(resultsPath, "/TopicDoc_network",getDate(),".png"), height=2500, width=2500, units="px")
+plot(net, 
+     vertex.size=1+(degree(net, V(net), mode="all", normalized=T)*40),
+     vertex.label.cex=1.5, 
+     vertex.label.color="black",
+     vertex.label=V(net)$TopicWords,vertex.color=as.character(color), 
+     vertex.shape=as.character(shape), 
+     edge.width=(edges$Weight*4.26)+0.957, edge.curved=0.2, 
+     layout=l, rescale=T)
+dev.off()
