ca1afedda5f5359e1ed194f106788458daafffa0
emilliman5 <emilliman4@gmail.com>
2015-09-26 11:51:28 -0400

    moved annotators outside loop

--- POStagging.R
+++ POStagging.R
@@ -5,17 +5,18 @@ library(tm)
 options(java.parameters = "-Xmx24000m")
 
 rm(abstrCorpus, pubdate.df,pub.file,pubmed, stopWords,top,myStopwords,grantID,grantNodes,nodes)
-
+Corpus<-abstrCorpus
 # convert corpus files into strings
 Corpus <- lapply(Corpus, function(x){
   x <- as.String(x)  }  )
+
+sent_token_annotator <- Maxent_Sent_Token_Annotator()
+word_token_annotator <- Maxent_Word_Token_Annotator()
+pos_tag_annotator <- Maxent_POS_Tag_Annotator()
 ###############################################################
 # Start actual PoS-tagging
 # apply annotators to Corpus
 Corpus.tagged <- lapply(Corpus, function(x){
-  sent_token_annotator <- Maxent_Sent_Token_Annotator()
-  word_token_annotator <- Maxent_Word_Token_Annotator()
-  pos_tag_annotator <- Maxent_POS_Tag_Annotator()
   y1 <- annotate(x, list(sent_token_annotator, word_token_annotator))
   y2 <- annotate(x, pos_tag_annotator, y1)
   # y3 <- annotate(x, Maxent_POS_Tag_Annotator(probs = TRUE), y1)
