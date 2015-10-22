3b731cc4d08f3ea624ab3cbe553a7c0016b2629c
Eric Milliman <emilliman4@gmail.com>
2015-10-22 17:34:06 -0400

    implemented Nvd3 barcharts

--- shiny/server.R
+++ shiny/server.R
@@ -1,5 +1,4 @@
 library(shiny)
-library(tm)
 library(reshape2)
 library(igraph)
 library(ape)
@@ -7,24 +6,8 @@ library(arcdiagram)
 library(RColorBrewer)
 library(riverplot)
 library(rCharts)
-library(topicmodels)
 library(dendextend)
 library(wordcloud)
-
-extraFunFile<-"textMine_funcs.R"
-if (file.exists(extraFunFile)) {
-    source(extraFunFile, keep.source=TRUE);
-}
-
-abstrCorpus<-Corpus(DirSource("../Corpus/"), readerControl = list(language="english"))
-metaData<-read.csv("../CorpusMetaData.txt",colClasses=c('character','character','Date','character','numeric'))
-for (x in c("PMID","GrantID","Date", "FY", "FY.Q")) {
-    meta(abstrCorpus, x)<-metaData[,x]
-}
-rm(metaData)
-
-tdm<-TermDocumentMatrix(abstrCorpus)
-load("../LDA_models_current.rda")
 
 #sankey<-function(){}
 
@@ -36,19 +19,26 @@ shinyServer(function(input,output) {
         lapply(input$fy, function(x)
             which(meta(abstrCorpus)$FY == x))})
     topicNames<-reactive({apply(terms(models[[as.integer(input$topicK)]],4),2,function(z) paste(z,collapse=","))})    
+    words<-reactive({unlist(strsplit(input$words, "\\s|,|;|\\t"))})
     output$wordcloud<-renderPlot({
         terms<-rowSums(as.matrix(tdm[,unlist(currentIds())]))
         terms<-terms[order(terms, decreasing = T)]
         wordcloud(names(terms), freq=terms,max.words = input$slider, colors=brewer.pal(9, "BuGn")[-(1:4)], random.order = F)
     })
-    output$topics<-renderPlot(height=800, width=1400, {
+    output$topics<-renderChart({
         #par(mar=c(20,8,5,2))
         #barplot(colSums(models[[as.integer(input$topicK)]]@gamma[unlist(currentIds()),]), las=2,names.arg = topicNames(), col=rainbow(10), ylab="Sum of Topic Probability")
-        gamma<-data.frame(topic=topicNames(), 
+        gamma<-data.frame(topic=rep(topicNames(),length(input$fy)), 
                           sum=unlist(lapply(currentIds(), 
-                                            function(x) colSums(models[[as.integer(input$topicK)]]@gamma[x,]) )), fy=rep(input$fy, each=models[[as.integer(input$topicK)]]@k))
+                                            function(x) colSums(models[[as.integer(input$topicK)]]@gamma[x,]) )), 
+                          fy=rep(input$fy, each=models[[as.integer(input$topicK)]]@k))
         p1<-nPlot(sum~topic, group="fy", data=gamma, type="multiBarChart")
         p1$addParams(dom="topics")
+        p1$chart(reduceXTicks = FALSE)
+        p1$xAxis(rotateLabels=-45)
         return(p1) 
         })
+    output$assoc<-renderText({
+        findAssocs(tdm[,unlist(currentIds())], input$words, input$corr)
+    })
 })
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,5 +1,6 @@
 library(shiny)
-load("../LDA_models_current.rda")
+library(rCharts)
+options(RCHART_LIB = 'polycharts')
 
 shinyUI(fluidPage(
     titlePanel("Kelly and Eric's super-awesome data mining adventure"),
@@ -9,21 +10,23 @@ shinyUI(fluidPage(
                      p("This will be a place to select your dataset, whether it 
                        be by FY, date range, Topic assignment, PMID, Grant ID, 
                        Journal"),
-                     fileInput("file",label=h3("File Upload"),accept="txt",),
+                     fileInput("file",label=h3("File Upload"),accept="txt"),
                      checkboxGroupInput("fy",selected = 2010,
                                         label=h3("Fiscal Years"),
                                         choices=list("ALL"=c(2009,2010,2011,2012,2013,2014,2015),"FY2009"=2009, "FY2010"=2010,
                                                      "FY2011"=2011,"FY2012"=2012,
                                                      "FY2013"=2013,"FY2014"=2014,
                                                      "FY2015"=2015)),
-                     radioButtons("topicK",label = "Topic Model Selection",choices = 
+                     radioButtons("topicK",selected = 2,label = "Topic Model Selection",choices = 
                                       list("25 Topics"=1,"50 Topics"=2,"100 Topics"=3,
                                            "250 Topics"=4,"500 Topics"=5,"1000 Topics"=6)),
-                     textInput("words",label = "Enter keywords here:")
+                     textInput("words",label = "Enter keywords here:",value = "")
                      ),
         mainPanel(
-            sliderInput("slider",label=h3("Max Number of Words"),min=10, max=500, value=75),
+            sliderInput("slider",label=h3("Max Number of Words"),min=10, max=500, value=25),
             plotOutput("wordcloud"),
-            plotOutput("topics")
+            showOutput("topics", "Nvd3"),
+            tableOutput("assoc"),
+            sliderInput("corr",label=h3("Minimum Correlation for Term associations"), min=0, max=1, value=0.3)
             ))
     ))
