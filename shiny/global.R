644ae43ba2d0aae7c093c2f13266c6200e5b387e
emilliman5 <emilliman4@gmail.com>
2015-11-12 07:48:18 -0500

    moved data files into shiny dir

--- shiny/global.R
+++ shiny/global.R
@@ -1,17 +1,17 @@
 library(tm)
 library(topicmodels)
 
-extraFunFile<-"../textMine_funcs.R"
+extraFunFile<-"textMine_funcs.R"
 if (file.exists(extraFunFile)) {
     source(extraFunFile, keep.source=TRUE);
 }
 
-abstrCorpus<-Corpus(DirSource("../Corpus/"), readerControl = list(language="english"))
-metaData<-read.csv("../CorpusMetaData.txt",colClasses=c('character','character','Date','character','numeric'))
+abstrCorpus<-Corpus(DirSource("data/Corpus/"), readerControl = list(language="english"))
+metaData<-read.csv("data/CorpusMetaData.txt",colClasses=c('character','character','Date','character','numeric'))
 for (x in c("PMID","GrantID","Date", "FY", "FY.Q")) {
     meta(abstrCorpus, x)<-metaData[,x]
 }
 rm(metaData)
 
 tdm<-TermDocumentMatrix(abstrCorpus)
-load("../LDA_models_current.rda")
+load("data/LDA_models_current.rda")
