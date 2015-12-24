e8f67f2e7cdb4164979836c6b76d6f4a63581f41
emilliman5 <emilliman4@gmail.com>
2015-12-24 11:40:28 -0500

    precomputed beta and gamma distances for dendroarcs

--- shiny/global.R
+++ shiny/global.R
@@ -1,5 +1,6 @@
 library(tm)
 library(topicmodels)
+library(proxy)
 
 extraFunFile<-"textMine_funcs.R"
 if (file.exists(extraFunFile)) {
@@ -16,3 +17,20 @@ rm(metaData)
 tdm<-TermDocumentMatrix(abstrCorpus)
 load("data/LDA_models_current.rda")
 load("data/LDA_FY_models_current.rda")
+
+betaDist<-lapply(models, function(x){
+    z<-as.matrix(x@beta)
+    rownames(z)<-apply(terms(x,4),2,function(y) paste(y,collapse=","))
+    simil(z, method="cosine") 
+})
+
+gammaDist<-lapply(models, function(x){
+    fys<-getFactorIdx("FY",meta(abstrCorpus))
+    lapply(fys, function(y){
+        simil(t(x@beta)[y,], method="cosine")
+    })    
+})
+
+betatrees<-lapply(betaDist, function(x){
+    hclust(x,method = "ward.d2")
+})
