4be5449179989143f3e915db4a79eb26468c9cc8
emilliman5 <emilliman4@gmail.com>
2015-12-25 06:55:06 -0500

    commented precomputing distance matrices until ready for implementation

--- shiny/global.R
+++ shiny/global.R
@@ -18,19 +18,19 @@ tdm<-TermDocumentMatrix(abstrCorpus)
 load("data/LDA_models_current.rda")
 load("data/LDA_FY_models_current.rda")
 
-betaDist<-lapply(models, function(x){
-    z<-as.matrix(x@beta)
-    rownames(z)<-apply(terms(x,4),2,function(y) paste(y,collapse=","))
-    simil(z, method="cosine") 
-})
-
-gammaDist<-lapply(models, function(x){
-    fys<-getFactorIdx("FY",meta(abstrCorpus))
-    lapply(fys, function(y){
-        simil(t(x@gamma)[,y], method="cosine")
-    })    
-})
-
-betatrees<-lapply(betaDist, function(x){
-    hclust(x,method = "ward.D2")
-})
+# betaDist<-lapply(models, function(x){
+#     z<-as.matrix(x@beta)
+#     rownames(z)<-apply(terms(x,4),2,function(y) paste(y,collapse=","))
+#     simil(z, method="cosine") 
+# })
+# 
+# gammaDist<-lapply(models, function(x){
+#     fys<-getFactorIdx("FY",meta(abstrCorpus))
+#     lapply(fys, function(y){
+#         simil(t(x@gamma)[,y], method="cosine")
+#     })    
+# })
+# 
+# betatrees<-lapply(betaDist, function(x){
+#     hclust(x,method = "ward.D2")
+# })
