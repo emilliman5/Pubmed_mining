73078c7747fb31da55dcb3becabb056786f2a40b
emilliman5 <emilliman4@gmail.com>
2015-12-28 17:16:33 -0500

    removed unused code.

--- shiny/global.R
+++ shiny/global.R
@@ -15,24 +15,6 @@ for (x in colnames(metaData)) {
 rm(metaData)
 
 tdm<-TermDocumentMatrix(abstrCorpus)
+
 load("data/LDA_models_current.rda")
 load("data/LDA_FY_models_current.rda")
-
-dict<-rownames(tdm)
-
-# betaDist<-lapply(models, function(x){
-#     z<-as.matrix(x@beta)
-#     rownames(z)<-apply(terms(x,4),2,function(y) paste(y,collapse=","))
-#     simil(z, method="cosine") 
-# })
-# 
-# gammaDist<-lapply(models, function(x){
-#     fys<-getFactorIdx("FY",meta(abstrCorpus))
-#     lapply(fys, function(y){
-#         simil(t(x@gamma)[,y], method="cosine")
-#     })    
-# })
-# 
-# betatrees<-lapply(betaDist, function(x){
-#     hclust(x,method = "ward.D2")
-# })
