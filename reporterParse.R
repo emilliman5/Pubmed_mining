24ceb614af62d7253550588418f2999f48844601
emilliman5 <emilliman4@gmail.com>
2015-12-29 22:10:48 -0500

    script to parse NIH reporter data for incorporation into the Corpus

---
+++ reporterParse.R
@@ -0,0 +1,16 @@
+process_NIH_reporter<-function(file){
+
+  library(tm)
+  library(lubridate)
+  library(parallel)
+
+  extraFunFile<-"textMine_funcs.R"
+  if (file.exists(extraFunFile)) {
+    source(extraFunFile, keep.source=TRUE);
+  }
+
+  table<-read.csv(file, skip = 4, stringsAsFactors = F)
+  table<-table[,c(2,3,4,6,9,10,37,42,46)] 
+  table[,1]<-gsub("DESCRIPTION \\(provided by applicant\\):","", table[,1])
+
+  }
