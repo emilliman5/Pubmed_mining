eb2c6f169e75c5e75ae969a89960a002a9195475
emilliman5 <emilliman4@gmail.com>
2015-12-29 22:14:17 -0500

    added some text removal operations

--- reporterParse.R
+++ reporterParse.R
@@ -12,5 +12,6 @@ process_NIH_reporter<-function(file){
   table<-read.csv(file, skip = 4, stringsAsFactors = F)
   table<-table[,c(2,3,4,6,9,10,37,42,46)] 
   table[,1]<-gsub("DESCRIPTION \\(provided by applicant\\):","", table[,1])
-
+  table[,1]<-gsub("Public Health Relevance:","", table[,1],ignore.case = T)
+  
   }
