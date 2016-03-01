d13687a41304f2b98620a60bf6029a1d9a52d28e
emilliman5 <emilliman4@gmail.com>
2016-03-01 17:51:16 -0500

    code to calculate distances between FY LDAs, needs to be expanded to all models

--- shiny/shiny_Init.R
+++ shiny/shiny_Init.R
@@ -23,6 +23,23 @@ grants.table<-data.frame(PMID=rep(metaData$PMID,
 write.table(grants.table, "data/PMIDs_to_grants.txt",col.names = T,sep="\t",quote=T)
 
 
-wordAssoc<-mclapply(split(rownames(tdm), cut(seq_along(rownames(tdm))), 50),
-                                           findAssocs(tdm,x, 0.01), mc.cores=4)
+wordAssoc<-mclapply(split(rownames(tdm), cut(seq_along(rownames(tdm)[1:5000]), 50)),
+                                           function(x) findAssocs(tdm,x, 0.01), mc.cores=4)
 
+
+
+##FY model-model distance measures.
+edges<-lapply(2:(length(models.fy)-1), function(x){
+    d<-dist(exp(models.fy[[x]][[2]]@beta), exp(models.fy[[(x+1)]][[2]]@beta), method="bhjattacharyya")
+    e<-dist2Table(d)
+    e$col<-paste0("FY",substr(names(models.fy)[x],3,4),"_",e$col)
+    e$row<-paste0("FY",substr(names(models.fy)[(x+1)],3,4),"_",e$row)
+    colnames(e)<-c("N1","N2","Value")
+    e
+})
+
+nodes<-do.call(rbind, lapply(seq_along(edges), function(x) {
+    n<-data.frame(ID=unique(edges[[x]]$N1), x=x, y=seq_along(unique(edges[[x]]$N1)))
+}))
+
+nodes<-rbind(nodes, data.frame(data.frame(ID=unique(edges[[6]]$N2), x=7, y=seq_along(unique(edges[[6]]$N2)))))
