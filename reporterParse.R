951df23389ad4164d5d7212c245d36b956a5437c
emilliman5 <emilliman4@gmail.com>
2016-03-22 13:21:17 -0400

    adjusted which fields to keep from NIH reporter, filled in and normalized columns with either blank or NA values.

--- reporterParse.R
+++ reporterParse.R
@@ -10,8 +10,10 @@ process_NIH_reporter<-function(file){
   }
 
   table<-read.csv(file, skip = 4, stringsAsFactors = F)
-  table<-table[,c(2,3,4,6,9,10,37,42,46)] 
+  table<-table[,c(2,3,4,5,6,9,10,18,19,20,37,38,39,42,46,47)]
+  table[table[,11]==" ",11]<-"NO"
+  table[is.na(table[,16]),16]<-0
   table[,1]<-gsub("DESCRIPTION \\(provided by applicant\\):","", table[,1])
   table[,1]<-gsub("Public Health Relevance:","", table[,1],ignore.case = T)
-  
+  table[,4]<-gsub("Project Narrative","", table[,4],ignore.case = T)
   }
