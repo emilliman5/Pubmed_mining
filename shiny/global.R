2f1d3068e380d4d1b9d552398669c2b0f8cb450c
emilliman5 <emilliman4@gmail.com>
2016-04-13 22:17:18 -0400

    removed corpus loading.

--- shiny/global.R
+++ shiny/global.R
@@ -1,8 +1,8 @@
-library(tm)
 library(topicmodels)
 library(proxy)
 library(ape)
 library(arcdiagram)
+library(slam)
 
 extraFunFile<-"shiny_textMine_funcs.R"
 if (file.exists(extraFunFile)) {
@@ -10,13 +10,7 @@ if (file.exists(extraFunFile)) {
     source("arcplot.r", keep.source=TRUE)
 }
 
-abstrCorpus<-Corpus(DirSource("data/Corpus/"), readerControl = list(language="english"))
-metaData<-read.csv("data/CorpusMetaData.txt",colClasses=c('character','character','Date','character','numeric'))
-for (x in colnames(metaData)) {
-    meta(abstrCorpus, x)<-metaData[,x]
-}
-rm(metaData)
-tdm<-TermDocumentMatrix(abstrCorpus)
+metaData<-read.csv("data/ModelMetaData.txt",colClasses=c('character','character','Date','character','numeric'))
 
 load("data/LDA_models_current.rda")
 load("data/beta.tree.rda")
